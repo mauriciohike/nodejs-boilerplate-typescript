9d84285099daffa9ce7ba332d0219f1b66722444
mauriciohike <mauriciorenanvs@gmail.com>
2020-05-09 19:18:01 -0300

    added prettier config

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => response.json({ message: 'first route' }));
+routes.get('/', (request, response) =>
+  response.json({ message: 'first route' }),
+);
 
 export default routes;
