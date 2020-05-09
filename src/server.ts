71ef85976bcaaca96526e8ec291603ff84de1a22
mauriciohike <mauriciorenanvs@gmail.com>
2020-05-09 19:08:01 -0300

    fix importing ts files

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.listen(3333, () =>{
+app.use(routes);
+
+app.listen(3333, () => {
   console.log('🛸 Server running');
 });
